efa6cf927376ae1e3ea1c1fc9a73eaa9d3cea5f1
Компьютер <@DESKTOP-0TR3ASG>
2023-12-02 12:09:17 +0700

    Добавьте файлы проекта.

---
+++ 5.3/5.3.cpp
@@ -0,0 +1,31 @@
+﻿/*Дан массив размера N (N – нечетное число). 
+Поменять местами его первый элемент со вторым, третий – с четвёртым и т.д.*/
+#include <iostream>
+using namespace std;
+// функция обмена местами
+void swap(int& a, int& b) 
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int[n];
+
+    for (int i = 0; i < n; i++)
+        // вводим массив
+        cin >> a[i];  
+    for (int i = 0; i < n - 1; i += 2)
+        //меняем местами элементы массива
+        swap(a[i], a[i + 1]);
+    for (int i = 0; i < n; i++)
+        // выводим результат
+        cout << a[i] << " "; 
+    return 0;
+}
+
+
